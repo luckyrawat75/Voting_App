064c0dfe5e5749c287db9e1be4f0391417208471
luckyrawat75 <rawataryan616@gmail.com>
2026-04-16 23:10:34 +0530

    fix: cors issue

--- Backend/index.js
+++ Backend/index.js
@@ -3,10 +3,7 @@ const app = express();
 require("dotenv").config();
 const db = require("./db");
 const cors = require("cors");
-
-app.use(cors({
-  origin: "http://localhost:5173",
-}));
+app.use(cors());
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
@@ -15,6 +12,10 @@ const PORT = process.env.PORT || 3200;
 
 const UserRoutes = require("./routes/userRoutes");
 const candidateRoutes = require("./routes/candidateRoutes");
+
+app.get("/", (req, res) => {
+  res.send("Voting API is running 🚀");
+});
 
 app.use("/user", UserRoutes);
 app.use("/candidate", candidateRoutes);
